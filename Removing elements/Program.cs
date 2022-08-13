23beadf249f3857f0e46d6612541650d257ea3ac
Julia Dubas <uliadubas94@gmail.com>
2022-08-13 16:10:40 +0300

    Add project files.

---
+++ Removing elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Removing_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = new string[] { "save", "delete", "save", "delete", "save", "delete" };
+
+            var listArray = myArray.ToList();
+
+            for (int i = listArray.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    listArray.RemoveAt(i);
+                }
+            }
+            string[] newArray = listArray.ToArray();
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
